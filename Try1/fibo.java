7abc7f67b01b94a1860c1ff75a943a2279dea941
mick243 <39043252+mick243@users.noreply.github.com>
2022-05-27 23:25:36 +0900

    Update fibo.java

--- Try1/fibo.java
+++ Try1/fibo.java
@@ -12,3 +12,20 @@ public class Main {
       System.out.println(fibo(4));
     }
  }
+//
+
+public class Main {
+  public static long[] d = new long[100];
+  
+  public static void main(String[] args) {
+    d[1] = 1;
+    d[2] = 1;
+    int n = 50;
+    
+    for (int i = 3, i <=n; i++) {
+        d[i] = d[i-1] + d[i-2];
+    }
+    System.out.println(d[n]);
+  }
+}
+//
